69455abd66284fc24d71387f7ad855ee37de3059
danialtien <tientung2029901@gmail.com>
2023-07-07 00:39:51 +0700

    Add Payment

--- app/src/main/java/com/trainh/assignmentprm/CartActivity.java
+++ app/src/main/java/com/trainh/assignmentprm/CartActivity.java
@@ -1,5 +1,6 @@
 package com.trainh.assignmentprm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,15 +12,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.paymentsheet.PaymentSheet;
+import com.stripe.android.paymentsheet.PaymentSheetResult;
+import com.stripe.net.ApiResource;
 import com.trainh.assignmentprm.adapter.CartAdapter;
 import com.trainh.assignmentprm.adapter.ProductAdapter;
 import com.trainh.assignmentprm.database.Database;
 import com.trainh.assignmentprm.entities.Product;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CartActivity extends AppCompatActivity implements CartAdapter.SelectedProduct{
 
@@ -29,6 +47,14 @@ public class CartActivity extends AppCompatActivity implements CartAdapter.Selec
     List<Product> productList;
     TextView txtMoney;
     Button btnThanhToan;
+
+    String PublishableKey = "";
+    String SecretKey = "";
+    String CustomerId;
+    String EphericalKey;
+    String ClientSecret;
+
+    PaymentSheet paymentSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +75,154 @@ public class CartActivity extends AppCompatActivity implements CartAdapter.Selec
         rvCart.setAdapter(cartAdapter);
         cartAdapter.notifyDataSetChanged();
 
+        //Payment
+        PaymentConfiguration.init(this, PublishableKey);
+        paymentSheet = new PaymentSheet(this,paymentSheetResult -> {
+            onPaymentResult(paymentSheetResult);
+        });
+
+
         btnThanhToan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                paymentFlow();
             }
         });
 
+
+        StringRequest request =new StringRequest(Request.Method.POST, "https://api.stripe.com/v1/customers",
+                new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject object = null;
+                try {
+                    object = new JSONObject(response);
+                    CustomerId = object.getString("id");
+//                    Toast.makeText(CartActivity.this, CustomerId, Toast.LENGTH_SHORT).show();
+                    getEmphericalKey();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(CartActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization", "Bearer " + SecretKey);
+                return header;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
+
+    private void paymentFlow() {
+        paymentSheet.presentWithPaymentIntent(ClientSecret,
+                new PaymentSheet.Configuration("Thanh toan", new PaymentSheet.CustomerConfiguration(
+                CustomerId,EphericalKey
+        )));
+    }
+
+    private void onPaymentResult(PaymentSheetResult paymentSheetResult) {
+        if (paymentSheetResult instanceof PaymentSheetResult.Completed) {
+            Toast.makeText(this, "Payment Success", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getEmphericalKey() {
+        StringRequest request =new StringRequest(Request.Method.POST, "https://api.stripe.com/v1/ephemeral_keys",
+                new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject object = null;
+                try {
+                    object = new JSONObject(response);
+                    EphericalKey = object.getString("id");
+//                    Toast.makeText(CartActivity.this, EphericalKey, Toast.LENGTH_SHORT).show();
+                    getClientSecret(CustomerId, EphericalKey);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(CartActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization", "Bearer " + SecretKey);
+                header.put("Stripe-Version","2022-11-15");
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer", CustomerId);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+
+    private void getClientSecret(String customerId, String ephericalKey) {
+
+        StringRequest request =new StringRequest(Request.Method.POST, "https://api.stripe.com/v1/payment_intents", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject object = null;
+                try {
+                    object = new JSONObject(response);
+                    ClientSecret = object.getString("client_secret");
+//                    Toast.makeText(CartActivity.this, ClientSecret, Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(CartActivity.this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                header.put("Authorization", "Bearer " + SecretKey);
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer", CustomerId);
+                params.put("amount", "100"+"00");
+                params.put("currency", "USD");
+                params.put("automatic_payment_methods[enabled]", "true");
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+
     private List<Product> getProductComputer() {
         List<Product> products = new ArrayList<Product>();
         Cursor dataProduct = database.GetData("SELECT cart.id, product.id, product.image, product.name, product.price, cart.quantity FROM cart INNER JOIN product ON cart.idProduct = product.id");
